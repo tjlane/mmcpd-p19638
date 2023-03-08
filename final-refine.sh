d4c951ef408e7c10aee865f2ab15619f6318b7b2
tjlane <tjlane@charmtx.com>
2023-03-08 17:14:34 +0000

    add occupancies to final refinement

--- final-refine.sh
+++ final-refine.sh
@@ -7,7 +7,9 @@ ${ttd_cif_path} \
 ${fda_cif_path} \
 xray_data.r_free_flags.file_name=${rfree_mtz_path} \
 main.number_of_macro_cycles=6 \
-refinement.refine.strategy=tls+individual_sites+individual_adp \
+refinement.refine.strategy=tls+individual_sites+individual_adp+occupancies \
 refinement.refine.adp.tls="chain 'A' and chain 'C' and chain 'D'" \
 refinement.refine.adp.tls="chain 'B' and chain 'F' and chain 'E'" \
 hydrogens.refine=riding
+
+# individual_sites_real_space
